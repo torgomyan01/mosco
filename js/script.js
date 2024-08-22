43c61c3bbe14a1be95c682b4703fce7c59003a98
Andranik Torgomyan <andranik@nativeteams.com>
2024-08-22 11:47:07 +0400

    End

--- js/script.js
+++ js/script.js
@@ -7,3 +7,35 @@ const {
 }
 
 AOS.init();
+
+
+function $(className){
+    return document.querySelectorAll(className)
+}
+
+const titleAnimation = $('.title-animation');
+
+titleAnimation.forEach((item) => {
+    const arr = item.dataset.titles.split(',');
+    let index = 0;
+    PrintItemsAnim(arr[index].split(''), item);
+
+    setInterval(() => {
+        index += 1;
+        const strArray = arr[index].split('');
+
+        PrintItemsAnim(strArray, item);
+
+        if(index === arr.length - 1){
+            index = 0
+        }
+    }, 6000)
+})
+
+function PrintItemsAnim(array, elem){
+    elem.innerHTML = '';
+    array.forEach((letter) => {
+        const span = `<span style="--letter: '${letter}'"></span>`
+        elem.insertAdjacentHTML('beforeend', span)
+    })
+}
